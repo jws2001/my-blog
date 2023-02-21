c2f62a28e7400e797cdc1f53253df82912ac9f5f
jws2001 <jws_17628484913@163.com>
2023-02-21 21:53:21 +0800

    依赖更新

--- client/vue3-blog/vite.config.js
+++ client/vue3-blog/vite.config.js
@@ -1,8 +1,15 @@
-export default {
-    proxy: {
-        '/web': {
-            target: 'http://songhua.icu',
-            changeOrigin: true,
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+
+export default defineConfig({
+    plugins: [vue()],
+    server: {
+        proxy: {
+            '/web': {
+                target: 'http://songhua.icu',
+                changeOrigin: true,
+            }
         }
     }
-}
+})
